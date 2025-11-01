0f90a5f1d1cd6e4ab4fd59d33386fa108c264e4e
nskai-org <nsukka.ai@gmail.com>
2025-11-01 17:22:47 +0100

    secure true

--- utils/generateTokenAndSetCookie.js
+++ utils/generateTokenAndSetCookie.js
@@ -7,7 +7,7 @@ export const generateTokenAndSetCookie = (res, userId, role) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    secure: true,
     sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
